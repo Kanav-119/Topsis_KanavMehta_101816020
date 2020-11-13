e5e75a2dc20727f7f8ed18cf138530e34c05b507
Kanav-119 <47269441+Kanav-119@users.noreply.github.com>
2020-11-13 23:13:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Arjun Malik',                   # Type in your name
   author_email = 'amalik_be18@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/ARJUN-MALIK/Topsis-ArjunMalik-101816029',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Topsis-ArjunMalik-101816029/archive/1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Kanav-119/Topsis_KanavMehta_101816020/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['SIMPLE', 'TOPSIS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'os',
